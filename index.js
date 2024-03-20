b65f5547d060e311f375c943cc915c8754713de6
mussaratshamsher <mussaratskhan@github.com>
2024-03-20 05:50:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answers = await inquirer.prompt({
+    message: "Enter Your age:", type: "number", name: "age"
+});
+console.log("InshaAllah! in \t" + (70 - answers.age) + " years you will be 70 years old.");
