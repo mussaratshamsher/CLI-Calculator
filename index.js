399ac29fd36c3599248806c3817c8e36ce34ca3a
mussaratshamsher <mussaratskhan@github.com>
2024-03-25 10:07:04 -0700

    used chalk in program

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 let answers = await inquirer.prompt({
     message: "Enter Your age:", type: "number", name: "age"
 });
-console.log("InshaAllah! in \t" + (70 - answers.age) + " years you will be 70 years old.");
+console.log(chalk.bold.green("InshaAllah! in \t" + (70 - answers.age) + " years you will be 70 years old."));
